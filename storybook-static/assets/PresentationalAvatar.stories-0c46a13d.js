9d7b1b58b53e30443e12e76e1522baaabbfff633
datsenkoboos <adacenkoboos@gmail.com>
2023-07-23 13:31:25 +0700

    built storybook for preview

---
+++ storybook-static/assets/PresentationalAvatar.stories-0c46a13d.js
@@ -0,0 +1,5 @@
+import{c as a}from"./vue.esm-bundler-4bf50236.js";import{_ as r,f as t}from"./index-97aabed2.js";const c={component:r},e=()=>a("div",{class:"flex gap-5"},[a(r,{username:"JohnDoe"},null),a(r,{username:"JohnDoe",image:t.image.avatar()},null)]);var o,n,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`() => <div class="flex gap-5">\r
+    <PresentationalAvatar username="JohnDoe" />\r
+    <PresentationalAvatar username="JohnDoe" image={faker.image.avatar()} />\r
+  </div>`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const l=["Presentation"];export{e as Presentation,l as __namedExportsOrder,c as default};
+//# sourceMappingURL=PresentationalAvatar.stories-0c46a13d.js.map
