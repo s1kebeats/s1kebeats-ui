915469ddb1a147d8819dd84e69c2714d5c4ee7f7
datsenkoboos <adacenkoboos@gmail.com>
2023-09-17 17:54:48 +0700

    Renamed CheckboxInput to SBCheckboxInput

---
+++ src/components/SBCheckboxInput/SBCheckboxInput.stories.tsx
@@ -0,0 +1,39 @@
+import SBCheckboxInput from './SBCheckboxInput.vue';
+import type { Meta } from '@storybook/vue3';
+
+const meta: Meta<typeof SBCheckboxInput> = {
+  component: SBCheckboxInput
+};
+
+export default meta;
+
+const SBCheckboxInputSizes: InstanceType<
+  typeof SBCheckboxInput
+>['$props']['size'][] = ['sm', 'md', 'lg', 'xl'];
+
+export const Presentation = () => (
+  <div class="flex flex-col gap-5 items-start">
+    {SBCheckboxInputSizes.map((size) => {
+      return (
+        <div class="flex gap-5">
+          <SBCheckboxInput name="input" size={size} key={size} />
+
+          <SBCheckboxInput
+            label="Receive notifications?"
+            name="input"
+            position="right"
+            size={size}
+            key={size + 'right'}
+          />
+          <SBCheckboxInput
+            label="Receive notifications?"
+            name="input"
+            position="left"
+            size={size}
+            key={size + 'left'}
+          />
+        </div>
+      );
+    })}
+  </div>
+);
