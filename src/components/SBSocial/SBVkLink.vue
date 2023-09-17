4a21ca07bff28328efe27dc60c600d00c063d71a
datsenkoboos <adacenkoboos@gmail.com>
2023-09-17 17:43:10 +0700

    Renamed Social components to SBSocial

---
+++ src/components/SBSocial/SBVkLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <SBSocialLink
+    url="https://vk.com/"
+    :username="username"
+    icon="mdi:vk"
+    :size="size"
+    data-testid="socialLink"
+  />
+</template>
+<script setup lang="ts">
+import SBSocialLink, { type SBSocialLinkProps } from './ui';
+
+const props = defineProps<Omit<SBSocialLinkProps, 'url' | 'icon'>>();
+</script>
---
+++ src/components/SBSocial/SBYoutubeLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <SBSocialLink
+    url="https://www.youtube.com/@"
+    :username="username"
+    icon="mdi:youtube"
+    :size="size"
+    data-testid="socialLink"
+  />
+</template>
+<script setup lang="ts">
+import SBSocialLink, { type SBSocialLinkProps } from './ui';
+
+const props = defineProps<Omit<SBSocialLinkProps, 'url' | 'icon'>>();
+</script>
